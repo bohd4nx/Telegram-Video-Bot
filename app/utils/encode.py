e90e90fb7a089006c61213188ecca01345b321e2
bohd4nx <bohd4n@proton.me>
2026-01-15 02:59:28 +0200

    fix: Add new_overlay directory to .gitignore and resolve unresolved attribute reference in encode.py

--- app/utils/encode.py
+++ app/utils/encode.py
@@ -10,7 +10,7 @@ def encode_segment(in_path: Path, output_path: Path, start_time: float, duration
     final_video = build_filters(input_video, files_dir)
 
     ffmpeg.output(
-        final_video.filter("format", "yuv420p"), input_video.audio, str(output_path),
+        final_video.filter("format", "yuv420p"), input_video.audio, str(output_path), # Unresolved attribute reference 'filter' for class 'FilterNode'
         vcodec="libx264", preset="fast", crf=20, pix_fmt="yuv420p",
         acodec="aac", audio_bitrate="128k", fpsmax=60, threads=0
     ).overwrite_output().run(capture_stdout=True, capture_stderr=True, quiet=True)
